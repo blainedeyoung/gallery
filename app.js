734ad6d5615cb137465f5e7334bc648bfa56c216
blainedeyoung <45204685+blainedeyoung@users.noreply.github.com>
2021-05-11 00:25:52 -0400

    another day

--- app.js
+++ app.js
@@ -3,22 +3,41 @@ const mainMenuB = document.getElementById("mainMenuButton");
 const aboutB = document.getElementById("aboutButton");
 const projectsB = document.getElementById("projectsButton");
 const linksB = document.getElementById("linksButton");
+const projectsListButton = document.getElementById("projectsListButton");
+const bootstrapSitesButton = document.getElementById("bootstrapSitesButton");
+const reactAppButton = document.getElementById("reactAppButton");
 
 //panels
 const topbar = document.getElementById("topbar");
+const viewer = document.getElementById("viewer");
 const mainMenu = document.getElementById("mainMenu");
 const about = document.getElementById("about");
-const projects = document.getElementById("projects");
+const projects = document.getElementById("projectsPanel");
 const links = document.getElementById("links");
 
+//project panels
+const projectsList = document.getElementById("projectsList");
+const bootstrapSitesList = document.getElementById("bootstrapSitesList");
+const reactAppList = document.getElementById("reactAppList");
+
 //return to main menu
-mainMenuB.onclick = () => {
+function returnToMainMenu() {
     topbar.innerHTML = "Main Menu";
+    viewer.style.display = "flex";
     about.style.display = "none";
     projects.style.display = "none";
     links.style.display = "none";
     mainMenuB.style.display = "none";
     mainMenu.style.display = "flex";
+};
+
+//display the projects menu
+projectsB.onclick = () => {
+    topbar.innerHTML = "Projects Page";
+    viewer.style.display = "block";
+    mainMenu.style.display = "none";
+    projects.style.display = "flex";
+    mainMenuB.style.display = "none";
 };
 
 //display the about
@@ -29,16 +48,6 @@ aboutB.onclick = () => {
     mainMenuB.style.display = "block";
 };
 
-//display the projects
-projectsB.onclick = () => {
-    topbar.innerHTML = "Projects Page";
-    mainMenu.style.display = "none";
-    projects.style.display = "flex";
-    mainMenuB.style.display = "block";
-    //might need this when we get more projects
-    //document.getElementById("viewer").style.overflow = 'scroll';
-};
-
 //display the links
 linksB.onclick = () => {
     topbar.innerHTML = "Links Page";
@@ -46,3 +55,33 @@ linksB.onclick = () => {
     links.style.display = "flex";
     mainMenuB.style.display = "block";
 };
+
+//display bootstrap sites list on projectsViewer
+projectsListButton.onclick = () => {
+  projectsList.style.display = "flex";
+  bootstrapSitesList.style.display = "none";
+  reactAppList.style.display = "none";
+  projectsListButton.style.boxShadow = "0 0 20px gold";
+  bootstrapSitesButton.style.boxShadow = "none";
+  reactAppButton.style.boxShadow = "none";
+}
+
+//display bootstrap sites list on projectsViewer
+bootstrapSitesButton.onclick = () => {
+  projectsList.style.display = "none";
+  bootstrapSitesList.style.display = "flex";
+  reactAppList.style.display = "none";
+  projectsListButton.style.boxShadow = "none";
+  bootstrapSitesButton.style.boxShadow = "0 0 20px gold";
+  reactAppButton.style.boxShadow = "none";
+}
+
+//display react app list on projectsViewer
+reactAppButton.onclick = () => {
+  projectsList.style.display = "none";
+  bootstrapSitesList.style.display = "none";
+  reactAppList.style.display = "flex";
+  projectsListButton.style.boxShadow = "none";
+  bootstrapSitesButton.style.boxShadow = "none";
+  reactAppButton.style.boxShadow = "0 0 20px gold";
+}
